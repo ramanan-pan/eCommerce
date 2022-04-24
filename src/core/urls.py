e5e544bfb16401a72bee74c2a1d121643a95446f
Michael Berejikian <michael.berejikian@uga.edu>
2022-04-23 21:46:47 -0400

    removed merge conflict text

--- src/core/urls.py
+++ src/core/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.conf import settings
 from django.conf.urls.static import static
 
-=======
 from website.admin import vendor_site, client_site
->>>>>>> 8a713bb60bd37f8ed82c32dd321276986433a3f2
 
 from website import views
 
